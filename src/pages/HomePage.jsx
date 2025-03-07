7d2b4bc7c65a68d01dd668342564f92366f0a9d2
Bhavya-Ayasya <bhavya.chawla@ayasya.com>
2025-03-07 13:26:33 +0530

    carrer component create

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import Careers from '../components/Careers'
 
 const HomePage = () => {
   return (
     <div>
       <Banner/>
+      <Careers/>
       HomePage
 
     </div>
