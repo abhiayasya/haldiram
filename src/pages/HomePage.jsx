8d134120de645ff278d4aeac73a8d6cb040c7a62
Abhishek Rajput <168184952+abhiayasya@users.noreply.github.com>
2025-03-07 17:27:00 +0530

    fix hero banner ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,11 @@ import Banner from "../components/Banner";
 import LatestNewsSection from "../components/LatestNewsSection";
 import BannerTeaser from "../components/BannerTeaser";
 import BrandSlider from "../components/slider/BrandSlider";
+import HeroSlider from "../components/slider/HeroSlider";
 const HomePage = () => {
   return (
     <div>
-      <Banner />
+      <HeroSlider/>
       <LatestNewsSection />
       <BannerTeaser />
       <section className="my-20">
