a1e8884f9cd8b1f2f1f5945263de99815d8803ce
Abhishek Rajput <168184952+abhiayasya@users.noreply.github.com>
2025-03-10 16:29:33 +0530

    max width container update

--- src/components/MaxWidthContainer.jsx
+++ src/components/MaxWidthContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MaxWidthContainer = ({ children, className, img }) => {
   return (
     <div
-      className={`${className} w-full my-4 md:my-8 lg:my-16 md:px-8 px-4`}
+      className={`${className} w-full md:px-8 px-4`}
       style={ img ? { backgroundImage: `url(${img})` }:{}}
     >
       <div className="max-w-[1280px] mx-auto">{children}</div>
